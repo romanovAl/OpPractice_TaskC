c376d02add9d8f18a42fa778362b6c93f64b835d
romanovAl <ml.naslednik2001@gmail.com>
2020-05-26 18:49:32 +0500

    Исправил косяки

--- Main.cpp
+++ Main.cpp
@@ -8,12 +8,12 @@ size_t findDistanceBetweenPositions(vector<string> const& text, TextPosition pos
 
 	size_t dist = 0;
 
-	for (int i = 0; i < size(text); i++) {
+	for (size_t i = 0; i < size(text); i++) {
 
-		for (int j = 0; j < size(text.at(i)); j++) {
+		for (size_t j = 0; j < size(text.at(i)); j++) {
 
 			TextPosition curPos(i, j);
-			if (curPos > pos1&& curPos < pos2) dist++;
+			if (curPos > pos1 && curPos < pos2) dist++;
 		}
 	}
 
@@ -27,13 +27,16 @@ size_t findFirstAndSec(vector<string> const& text, char ch1, char ch2) {
 	TextPosition ch2Pos(0, 0);
 
 	//поиск наименьшего вхождения первого символа и наибольшего вхождения второго
-	for (int i = 0; i < size(text); i++) {
+	for (size_t i = 0; i < size(text); i++) {
 
 		auto& x = text.at(i);
 
 
 		size_t firstOf = x.find_first_of(ch1);
 		size_t lastOf = x.find_last_of(ch2);
+
+		if (lastOf < firstOf) return string::npos;
+		if (lastOf < firstOf) return 0;
 
 
 		if (firstOf != string::npos) {
@@ -68,11 +71,15 @@ int testFun() {
 
 	"wow lol kek cheburek w ko" };
 
+	vector<string> const& test = { "1", "2", "3", "4", "5", "6", "7" };
+
 	if (findFirstAndSec(text, 'e', 'e') != 58) return 1;
 	if (findFirstAndSec(text, 'A', 'n') != 13) return 2;
 	if (findFirstAndSec(text, '!', '3') != 15) return 3;
 	if (findFirstAndSec(text, ' ', ' ') != 60) return 4;
 	if (findFirstAndSec(text, 'w', 'k') != 22) return 5;
+	if (findFirstAndSec(test, '1', '1') != 0) return 6;
+	if (findFirstAndSec(test, '7', '2') != string::npos) return 7; 
 
 	return 0;
 }
